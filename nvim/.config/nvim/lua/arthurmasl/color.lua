67dea4287501028ac0950ea606adb5bf30d95a4c
Artur Maslobojev <arthurmasl@gmail.com>
2022-09-22 19:34:13 +0300

    change cursor line color

--- nvim/.config/nvim/lua/arthurmasl/color.lua
+++ nvim/.config/nvim/lua/arthurmasl/color.lua
@@ -15,6 +15,7 @@ vim.cmd("colorscheme nordfox")
 
 vim.api.nvim_set_hl(0, "LineNr", { bg = "None", fg = "#6b717d" })
 vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "None", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#32394a" })
 
 -- vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3f495a" })
 -- vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3f495a" })
