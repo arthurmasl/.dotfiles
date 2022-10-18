ca5284c7fd5c005dc6ef32733cbef5a8bf032fd2
Artur Maslobojev <arthurmasl@gmail.com>
2022-10-18 15:21:34 +0300

    lsp cap

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -6,7 +6,7 @@ local rt = require("rust-tools")
 local lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 vim.diagnostic.config({
 	virtual_text = true,
