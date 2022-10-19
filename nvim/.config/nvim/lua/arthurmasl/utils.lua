5415bb32933bba351d185c4194d450aef47295a4
Artur Maslobojev <arthurmasl@gmail.com>
2022-10-19 21:50:00 +0300

    go to definition list

--- nvim/.config/nvim/lua/arthurmasl/utils.lua
+++ nvim/.config/nvim/lua/arthurmasl/utils.lua
@@ -14,3 +14,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+local function filterReactDTS(value)
+	return string.match(value.filename, "react/index.d.ts") == nil
+end
+
+local function on_list(options)
+	local items = options.items
+	if #items > 1 then
+		items = filter(items, filterReactDTS)
+	end
+
+	vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
+	vim.api.nvim_command("cfirst")
+end
