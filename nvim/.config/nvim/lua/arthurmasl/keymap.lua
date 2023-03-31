16562f4ee753b949642cae02e31780c81af7aa20
Artur Maslobojev <arthurmasl@gmail.com>
2023-03-31 17:42:34 +0300

    comment alternative file

--- nvim/.config/nvim/lua/arthurmasl/keymap.lua
+++ nvim/.config/nvim/lua/arthurmasl/keymap.lua
@@ -21,7 +21,7 @@ map("n", "<Leader>e", ":NvimTreeFindFileToggle<CR>")
 map("n", "<Leader>E", ":NvimTreeFindFile<CR>")
 
 -- alternative file
-map("n", "<C-O>", ":e #<CR>")
+-- map("n", "<C-S-o>", ":e #<CR>")
 
 -- toggles
 map("n", "<Leader>ts", ":set spell!<CR>")
