d098d724a4bf5e70d4e72ba049e621ce444efc10
Artur Maslobojev <arthurmasl@gmail.com>
2023-05-24 19:49:20 +0300

    update

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -7,6 +7,7 @@ local lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+capabilities.offsetEncoding = "utf-8"
 
 vim.diagnostic.config({
 	virtual_text = true,
