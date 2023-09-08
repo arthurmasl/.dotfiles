17d1fda21300039bcb29a18368824bfc13d7add5
Artur Maslobojev <arthurmasl@gmail.com>
2023-09-08 17:22:30 +0300

    refactoring

--- nvim/.config/nvim/after/plugin/spectre.lua
+++
@@ -1,9 +0,0 @@
--- require("spectre").setup({
--- 	mapping = {
--- 		-- ["send_to_qf"] = {
--- 		-- 	map = "<leader>f",
--- 		-- 	cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
--- 		-- 	desc = "send all item to quickfix",
--- 		-- },
--- 	},
--- })
