baba7e5e639cd76ebd5013903c6ce1f89d0a8378
Artur Maslobojev <arthurmasl@gmail.com>
2023-09-08 17:13:44 +0300

    tree

---
+++ nvim/.config/nvim/after/plugin/tree.lua
@@ -0,0 +1,68 @@
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	vim.keymap.set("n", "<Up>", api.node.show_info_popup, opts("Info"))
+	vim.keymap.set("n", "<C-g>", api.tree.change_root_to_node, opts("CD"))
+	vim.keymap.set("n", "<C-s>", api.node.run.system, opts("Run System"))
+end
+
+require("nvim-tree").setup({
+	on_attach = on_attach,
+	sync_root_with_cwd = true,
+	respect_buf_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+	},
+	renderer = {
+		icons = {
+			webdev_colors = false,
+			show = {
+				file = false,
+				folder = false,
+				folder_arrow = false,
+				git = true,
+			},
+			glyphs = {
+				git = {
+					unstaged = "US",
+					staged = "S",
+					unmerged = "UM",
+					renamed = "R",
+					untracked = "UT",
+					deleted = "D",
+					ignored = "I",
+				},
+			},
+		},
+	},
+	view = {
+		adaptive_size = true,
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = false,
+		debounce_delay = 50,
+		icons = {
+			hint = "H",
+			info = "I",
+			warning = "W",
+			error = "E",
+		},
+	},
+	git = {
+		show_on_dirs = false,
+	},
+	filters = {
+		dotfiles = false,
+		custom = { "^\\.DS_Store" },
+		-- custom = { "^\\.git", "^\\.DS_Store" },
+	},
+})
