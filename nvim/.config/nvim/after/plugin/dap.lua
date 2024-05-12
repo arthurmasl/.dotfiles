407c4eac87d4fe0e7f36997078a1e2ee5938d26b
Artur Maslobojev <arthurmasl@gmail.com>
2024-05-12 18:22:26 +0300

    formatting

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -66,17 +66,9 @@ dap.configurations.odin = {
 	},
 }
 
-require("dap-vscode-js").setup({
-	-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-	-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-	-- debugger_path = "/Users/arthurmasl/Work/vscode-js-debug", -- Path to vscode-js-debug installation.
-	-- debugger_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter',
-	-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-	adapters = { "pwa-node", "pwa-chrome", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-	-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-	-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-	-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-})
+-- require("dap-vscode-js").setup({
+-- 	adapters = { "pwa-node", "pwa-chrome", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+-- })
 
 for _, language in ipairs({ "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" }) do
 	dap.configurations[language] = {
