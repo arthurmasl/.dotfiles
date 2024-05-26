b344c6c8f4944b105e130d0f8cbf9f3ee6d49a72
Artur Maslobojev <arthurmasl@gmail.com>
2024-05-26 23:10:26 +0300

    toggle term

--- nvim/.config/nvim/after/plugin/terminal.lua
+++ nvim/.config/nvim/after/plugin/terminal.lua
@@ -6,12 +6,12 @@ require("toggleterm").setup({
 	size = 15,
 	persist_mode = false,
 	persist_size = false,
-  start_in_insert = true,
 	-- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
 })
 
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 vim.keymap.set("t", "<C-o>", [[<Cmd>wincmd p<CR>]], opts)
+vim.keymap.set("n", "<Leader>/", ":ToggleTerm<CR><Cmd>wincmd p<CR>", opts)
 
 vim.cmd([[nnoremap <silent><c-_> <Cmd>exe v:count1 . "ToggleTerm"<CR>]])
 vim.cmd([[inoremap <silent><c-_> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>]])
