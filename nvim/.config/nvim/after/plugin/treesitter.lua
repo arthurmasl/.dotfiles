47bf261b0de8f6389e52e8ebddebf105876f0604
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-16 19:18:10 +0200

    move

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -74,28 +74,36 @@ require("nvim-treesitter.configs").setup({
 				-- / comment (text)
 				["a/"] = "@comment.outer",
 			},
-			-- You can choose the select mode (default is charwise 'v')
-			--
-			-- Can also be a function which gets passed a table with the keys
-			-- * query_string: eg '@function.inner'
-			-- * method: eg 'v' or 'o'
-			-- and should return the mode ('v', 'V', or '<c-v>') or a table
-			-- mapping query_strings to modes.
 			selection_modes = {
 				["@parameter.outer"] = "v", -- charwise
 				["@function.outer"] = "V", -- linewise
 				["@class.outer"] = "<c-v>", -- blockwise
 			},
-			-- If you set this to `true` (default is `false`) then any textobject is
-			-- extended to include preceding or succeeding whitespace. Succeeding
-			-- whitespace has priority in order to act similarly to eg the built-in
-			-- `ap`.
-			--
-			-- Can also be a function which gets passed a table with the keys
-			-- * query_string: eg '@function.inner'
-			-- * selection_mode: eg 'v'
-			-- and should return true or false
 			include_surrounding_whitespace = true,
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]f"] = "@function.outer",
+				["]c"] = "@class.outer",
+				["]i"] = "@conditional.outer",
+				["]l"] = "@loop.outer",
+				["]a"] = "@parameter.inner",
+				["]o"] = "@call.inner",
+				["]e"] = "@assignment.outer",
+				["]/"] = "@comment.outer",
+			},
+			goto_previous_start = {
+				["[f"] = "@function.outer",
+				["[c"] = "@class.outer",
+				["[i"] = "@conditional.outer",
+				["[l"] = "@loop.outer",
+				["[a"] = "@parameter.inner",
+				["[o"] = "@call.inner",
+				["[e"] = "@assignment.outer",
+				["[/"] = "@comment.outer",
+			},
 		},
 	},
 })
