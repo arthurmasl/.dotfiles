fccd36f4d5ecae07882ab33d13c70951039130c6
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-06 15:29:03 +0200

    underline

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -16,7 +16,7 @@ vim.diagnostic.config({
 	virtual_text = true,
 	signs = true,
 	update_in_insert = true,
-	underline = false,
+	underline = true,
 	severity_sort = false,
 	float = {
 		header = "",
